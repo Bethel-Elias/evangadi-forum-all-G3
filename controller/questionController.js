565e4359c1007dd8d6879d085e7c04a919f9c0cb
abiya-gilo <bekabiya@gmail.com>
2026-01-09 18:55:42 -0500

    ahmed-single-question

--- controller/questionController.js
+++ controller/questionController.js
@@ -37,4 +37,52 @@ async function askQuestion(req, res) {
 
 
 
+
+
+
+
+
+
+
+
+
+async function singleQuestion(req, res) {
+  const { id } = req.params;
+
+  try {
+    //get question
+    const [rows] = await dbconnection.query(
+      `SELECT q.questionid,q.title,q.description,q.created_at, u.username
+       FROM questions_Table q
+      JOIN users_Table u ON q.userid = u.userid
+       WHERE q.questionid = ?`,
+      [id]
+    );
+
+    if (rows.length === 0) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ msg: "The request question could not be found." });
+    }
+
+    const question = rows[0];
+
+    //get answer for this question
+    const [answers] = await dbconnection.query(
+      `SELECT a.answerid, a.answer, a.created_at, u.username
+       FROM answers_table a
+       JOIN users_Table u ON a.userid = u.userid
+       WHERE a.questionid = ?
+       ORDER BY a.created_at DESC`,
+      [id]
+    );
+
+    res.status(StatusCodes.OK).json({ question, answers });
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "An unexpected error occurred" });
+  }
+}
+
 module.exports = { askQuestion, allQuestions, singleQuestion };
