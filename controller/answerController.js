29919c54a4943052824422dc14fd0dbfda6236b8
mohammed-abDev <mohammedabdudev11@gmail.com>
2026-01-11 07:55:53 +0300

    add answer post

--- controller/answerController.js
+++ controller/answerController.js
@@ -1,5 +1,5 @@
 // Import database connection
-const db = require("../db/dbconfig");
+const dbconnection = require("../db/dbconfig");
 // Import status codes
 const { StatusCodes } = require("http-status-codes");
 // Import UUID for unique identifiers
@@ -7,41 +7,47 @@ const { v4: uuidv4 } = require("uuid");
 
 
 //============ Post (Create) Answer Controller ============//
-const postAnswer = async (req, res) => {
-    const { questionid, answer } = req.body;
+async function postAnswer(req, res) {
+    const { answer, questionid } = req.body;
+    const userid = req.user.userid;
 
-    // comes from auth middleware
-    const userid = req.user.userid; 
-    const username = req.user.username;
+    if (!answer || !questionid) {
 
-  // Validate required fields
-    if (!answer) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            error: "Bad Request",
-            message: "Please provide answer",
-        });
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .json({ msg: "Please provide answers" });
     }
 
-    try {
-        await dbConnection.query(
-        "INSERT INTO answer_table (answerid,userid, questionid, answer) VALUES (?, ?, ?, ?)",
-        [uuidv4(),userid, questionid, answer]
+    if (!userid) {
+        return res
+            .status(StatusCodes.UNAUTHORIZED)
+            .json({ msg: "Authentication required" });
+    }
+
+try {
+    const [result] = await dbconnection.query(
+        "INSERT INTO answers_table (questionid, userid, answer) VALUES (?,?,?)",
+        [questionid, userid, answer]
     );
 
-    return res.status(StatusCodes.CREATED).json({
-        message: "Answer created successfully.",
-        answer: {
-            userid,questionid,answer,username,created_at: new Date(),
-            }
-    });
-    } catch (error) {
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-        error: "Internal Server Error",
-        message: "An unexpected error occurred.",
-        });
-    }
-};
+    const [newAnswer] = await dbconnection.query(
+        `SELECT 
+        a.answerid,
+        a.answer,
+        a.created_at,
+        u.username
+    FROM answers_table a
+    JOIN users_Table u ON a.userid = u.userid
+    WHERE a.answerid = ?`,
+    [result.insertId]
+    );
 
-module.exports = {
-    postAnswer,
-};
+    res.status(201).json({ answer: newAnswer[0] });
+
+    res.status(StatusCodes.CREATED).json({ msg: "Answer posted successfully" });
+} catch (error) {
+    res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ msg: "An unexpected error occurred." });
+    }
+}
