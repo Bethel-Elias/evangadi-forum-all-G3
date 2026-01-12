4300adffaebf50a4a280fb6c310e49c8897a4f65
Bethel-Elias <betineba1@gmail.com>
2026-01-12 10:32:04 +0300

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -1,3 +1,7 @@
+const dbconnection = require("../db/dbconfig");
+const bcrypt = require("bcrypt");
+const {StatusCodes} =require("http-status-codes")
+const jwt = require("jsonwebtoken")
 
 
 //register
