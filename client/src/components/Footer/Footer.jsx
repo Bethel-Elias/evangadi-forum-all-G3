e7d9bf9a59613801a5da445fc98f381a8fb23a16
Lengereh777 <amanuelkebede555@gmail.com>
2026-01-13 01:52:53 +0100

    Added Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -7,9 +7,45 @@ import style from "./footer.module.css";
 
 function Footer() {
   return (
-    <>
-      
-    </>
+    <div className={style.footer_container}>
+        <div>
+          <img src="/10001.png" alt="" />
+          <br />
+          <span>
+            <FaFacebook />
+            <FaInstagram />
+            <FaYoutube />
+          </span>
+        </div>
+        <div>
+          <h2>Useful Link</h2>
+          <ul>
+            <li>
+              <Link to="#">How It works</Link>
+            </li>
+            <li>
+              <Link to="#">Terms of Service</Link>
+            </li>
+            <li>
+              <Link to="#">Privacy Policy</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Contact Info</h2>
+          <ul>
+            <li>
+              <Link to="#">Evangadi Networks</Link>
+            </li>
+            <li>
+              <Link to="#">Support@evangadi.com</Link>
+            </li>
+            <li>
+              <Link to="">+1-202-386-2702</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
   );
 }
 
